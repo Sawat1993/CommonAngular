0dfd218421493fdcf89fa660debdf7ab8531961e
Sawatantra Chauhan <Sawatantra>
2019-09-10 01:31:33 +0530

    adding service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 /** Importing Components */
 import { AppComponent } from './app.component';
@@ -12,6 +13,10 @@ import { AppRoutingModule } from './/app-routing.module';
 import { FeatureModule } from './feature/feature.module';
 import { SharedModule } from './shared/shared.module';
 
+/** Import Services */
+import { AsyncHttpService } from './provider/async-http.service';
+import { TokenInterceptor } from './provider/http-interceptor.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +24,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FeatureModule,
     SharedModule
--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncHttpService } from '../../provider/async-http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private asyncHttpService: AsyncHttpService) { }
 
   ngOnInit() {
+    this.asyncHttpService.get('./assets/data/data.json').subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
