a835dcb1c7cee3069047e11e3c3a5288f2b5815f
Sawatantra Chauhan <Sawatantra>
2019-09-10 00:57:57 +0530

    adding documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/** Importing Components */
 import { AppComponent } from './app.component';
+import { MainComponent } from './main.component';
+
+/**
+ * Importing Modules
+ */
 import { AppRoutingModule } from './/app-routing.module';
 import { FeatureModule } from './feature/feature.module';
 import { SharedModule } from './shared/shared.module';
-import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [
